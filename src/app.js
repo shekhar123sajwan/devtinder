807ea7097f92bcce262caf132b37ddfc6bbd4a25
Shekhar Sajwan <shekharsajwan97@gmail.com>
2024-09-17 10:55:52 +0530

    first  commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("HELLO");
+});
+
+app.get("/x", (req, res) => {
+  res.send("HELLO");
+});
+
+app.listen(3000, () => {
+  console.log("Server listening port 3000");
+});
