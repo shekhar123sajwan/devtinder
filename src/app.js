4716fba9473c0ef515b4a84c58bcf2aa4a62b571
Shekhar Sajwan <shekharsajwan97@gmail.com>
2024-09-17 11:39:41 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("HELLO");
-});
+// app.get(/[a-z][0-9]{0,9}$/, (req, res) => {
+//   res.send({ name: "SHEKHAR" });
+// });
 
-app.get("/x", (req, res) => {
-  res.send("HELLO");
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+  res.send("HELLo");
 });
 
 app.listen(3000, () => {
